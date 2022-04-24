8d8389a819ba1b5070bc5b5bc7357442b55bb9fa
Ofek Elgozi <ofeke209@gmail.com>
2022-04-24 18:18:10 +0300

    Added Some Functions To Code

---
+++ Assignment2.py
@@ -0,0 +1,161 @@
+def PrintMatrix(matrix):
+    """
+    Matrix Printing Function
+    :param matrix: Matrix nxn
+    """
+    for line in matrix:
+        print('  '.join(map(str, line)))
+
+
+def MaxNorm(matrix):
+    """
+    Function for calculating the max-norm of a matrix
+    :param matrix: Matrix nxn
+    :return:max-norm of a matrix
+    """
+    max_norm = 0
+    for i in range(len(matrix)):
+        norm = 0
+        for j in range(len(matrix)):
+            # Sum of organs per line with absolute value
+            norm += abs(matrix[i][j])
+        # Maximum row amount
+        if norm > max_norm:
+            max_norm = norm
+
+    return max_norm
+
+
+def Determinant(matrix, mul):
+    """
+    Recursive function for determinant calculation
+    :param matrix: Matrix nxn
+    :param mul: The double number
+    :return: determinant of matrix
+    """
+    width = len(matrix)
+    # Stop Conditions
+    if width == 1:
+        return mul * matrix[0][0]
+    else:
+        sign = -1
+        det = 0
+        for i in range(width):
+            m = []
+            for j in range(1, width):
+                buff = []
+                for k in range(width):
+                    if k != i:
+                        buff.append(matrix[j][k])
+                m.append(buff)
+            # Change the sign of the multiply number
+            sign *= -1
+            #  Recursive call for determinant calculation
+            det = det + mul * Determinant(m, sign * matrix[0][i])
+    return det
+
+
+def MakeIMatrix(cols, rows):
+    # Initialize a identity matrix
+    return [[1 if x == y else 0 for y in range(cols)] for x in range(rows)]
+
+
+def MultiplyMatrix(matrixA, matrixB):
+    """
+    Function for multiplying 2 matrices
+    :param matrixA: Matrix nxn
+    :param matrixB: Matrix nxn
+    :return: Multiplication between 2 matrices
+    """
+    # result matrix initialized as singularity matrix
+    result = [[0 for y in range(len(matrixB[0]))] for x in range(len(matrixA))]
+    for i in range(len(matrixA)):
+        # iterate through columns of Y
+        for j in range(len(matrixB[0])):
+            # iterate through rows of Y
+            for k in range(len(matrixB)):
+                result[i][j] += matrixA[i][k] * matrixB[k][j]
+    return result
+
+
+def InverseMatrix(matrix, vector):
+    """
+    Function for calculating an inverse matrix
+    :param matrix:  Matrix nxn
+    :return: Inverse matrix
+    """
+    # Unveri reversible matrix
+    if Determinant(matrix, 1) == 0:
+        print("Error,Singular Matrix\n")
+        return
+    # result matrix initialized as singularity matrix
+    result = MakeIMatrix(len(matrix), len(matrix))
+    # loop for each row
+    for i in range(len(matrix[0])):
+        # turn the pivot into 1 (make elementary matrix and multiply with the result matrix )
+        # pivoting process
+        matrix, vector = RowXchange(matrix, vector)
+        elementary = MakeIMatrix(len(matrix[0]), len(matrix))
+        elementary[i][i] = 1 / matrix[i][i]
+        result = MultiplyMatrix(elementary, result)
+        matrix = MultiplyMatrix(elementary, matrix)
+        # make elementary loop to iterate for each row and subtracrt the number below (specific) pivot to zero  (make
+        # elementary matrix and multiply with the result matrix )
+        for j in range(i + 1, len(matrix)):
+            elementary = MakeIMatrix(len(matrix[0]), len(matrix))
+            elementary[j][i] = -(matrix[j][i])
+            matrix = MultiplyMatrix(elementary, matrix)
+            result = MultiplyMatrix(elementary, result)
+
+    # after finishing with the lower part of the matrix subtract the numbers above the pivot with elementary for loop
+    # (make elementary matrix and multiply with the result matrix )
+    for i in range(len(matrix[0]) - 1, 0, -1):
+        for j in range(i - 1, -1, -1):
+            elementary = MakeIMatrix(len(matrix[0]), len(matrix))
+            elementary[j][i] = -(matrix[j][i])
+            matrix = MultiplyMatrix(elementary, matrix)
+            result = MultiplyMatrix(elementary, result)
+
+    return result
+
+
+def InverseMatrix(matrix):
+    """
+    Function for calculating an inverse matrix
+    :param matrix:  Matrix nxn
+    :return: Inverse matrix
+    """
+    vector=[0]*len(matrix)
+    # Unveri reversible matrix
+    if Determinant(matrix, 1) == 0:
+        print("Error,Singular Matrix\n")
+        return
+    # result matrix initialized as singularity matrix
+    result = MakeIMatrix(len(matrix), len(matrix))
+    # loop for each row
+    for i in range(len(matrix[0])):
+        # turn the pivot into 1 (make elementary matrix and multiply with the result matrix )
+        # pivoting process
+        matrix, vector = RowXchange(matrix, vector)
+        elementary = MakeIMatrix(len(matrix[0]), len(matrix))
+        elementary[i][i] = 1 / matrix[i][i]
+        result = MultiplyMatrix(elementary, result)
+        matrix = MultiplyMatrix(elementary, matrix)
+        # make elementary loop to iterate for each row and subtracrt the number below (specific) pivot to zero  (make
+        # elementary matrix and multiply with the result matrix )
+        for j in range(i + 1, len(matrix)):
+            elementary = MakeIMatrix(len(matrix[0]), len(matrix))
+            elementary[j][i] = -(matrix[j][i])
+            matrix = MultiplyMatrix(elementary, matrix)
+            result = MultiplyMatrix(elementary, result)
+
+    # after finishing with the lower part of the matrix subtract the numbers above the pivot with elementary for loop
+    # (make elementary matrix and multiply with the result matrix )
+    for i in range(len(matrix[0]) - 1, 0, -1):
+        for j in range(i - 1, -1, -1):
+            elementary = MakeIMatrix(len(matrix[0]), len(matrix))
+            elementary[j][i] = -(matrix[j][i])
+            matrix = MultiplyMatrix(elementary, matrix)
+            result = MultiplyMatrix(elementary, result)
+
+    return matrix
