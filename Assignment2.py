aaf312d70915ffe6da7b1a6300579f09c22b8ab8
Stav <stavshar21@gmail.com>
2022-04-24 18:22:47 +0300

    adding some functions

--- Assignment2.py
+++ Assignment2.py
@@ -159,3 +159,161 @@ def InverseMatrix(matrix):
             result = MultiplyMatrix(elementary, result)
 
     return matrix
+
+
+def RowXchange(matrix, vector):
+    """
+    Function for replacing rows with both a matrix and a vector
+    :param matrix: Matrix nxn
+    :param vector: Vector n
+    :return: Replace rows after a pivoting process
+    """
+
+    for i in range(len(matrix)):
+        max = abs(matrix[i][i])
+        for j in range(i, len(matrix)):
+            # The pivot member is the maximum in each column
+            if abs(matrix[j][i]) > max:
+                temp = matrix[j]
+                temp_b = vector[j]
+                matrix[j] = matrix[i]
+                vector[j] = vector[i]
+                matrix[i] = temp
+                vector[i] = temp_b
+                max = abs(matrix[i][i])
+
+    return [matrix, vector]
+
+
+def CheckDominantDiagonal(matrix):
+    """
+    Function for testing a dominant diagonal
+    :param matrix: Matrix nxn
+    :return: True or false if there is a domene diagonal
+    """
+    for i in range(len(matrix)):
+        sum = 0
+        for j in range(len(matrix)):
+            if i != j:
+                # Sum of line member without pivot
+                sum += abs(matrix[i][j])
+        # If the pivot is less than the sum of the rest of the line
+        if abs(matrix[i][i]) < sum:
+            return False
+    return True
+
+
+def DominantDiagonalFix(matrix):
+    """
+    Function to change a matrix to create a dominant diagonal
+    :param matrix: Matrix nxn
+    :return: Change the matrix to a dominant diagonal
+    """
+    #Check if we have a dominant for each column
+    dom = [0]*len(matrix)
+    result = list()
+   # Find the largest organ in a row
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if (matrix[i][j] > sum(map(abs,map(int,matrix[i])))-matrix[i][j]) :
+                dom[i]=j
+    for i in range(len(matrix)):
+        result.append([])
+        # Cannot dominant diagonal
+        if i not in dom:
+            print("Couldn't find dominant diagonal.")
+            return matrix
+    # Change the matrix to a dominant diagonal
+    for i,j in enumerate(dom):
+        result[j]=(matrix[i])
+    return result
+
+
+def minusMatrix(matrix):
+    """
+    :param matrix: Matrix nxn
+    :return: Subtract between matrices
+    """
+    return [[-i for i in j] for j in matrix]
+
+
+def matrixAddition(matrixA, matrixB):
+    """
+       :param matrix: Matrix nxn, Matrix nxn
+       :return: Addition between matrices
+       """
+    return [[a + b for (a, b) in zip(i, j)] for (i, j) in zip(matrixA, matrixB)]
+
+
+
+def matrixDLUdissasembly(matrix):
+    """
+
+    :param matrix: matrix nXn
+    :return: Breaking down matrices L U D
+    """
+    D, L, U = list(), list(), list()
+    for x, row in enumerate(matrix):
+        D.append(list()), L.append(list()), U.append(list())
+        for y, value in enumerate(row):
+            # Diagonal with zeros
+            if x == y:
+                D[x].append(value), L[x].append(0), U[x].append(0)
+            # Zeros below diagonal
+            elif x < y:
+                D[x].append(0), L[x].append(0), U[x].append(value)
+            # Zeros above diagonal
+            elif x > y:
+                D[x].append(0), L[x].append(value), U[x].append(0)
+    return D, L, U
+
+
+def JacobiG(matrix):
+    """
+    :param matrix: Matrix nxn
+    :return: G matrix
+    """
+    D, L, U = matrixDLUdissasembly(matrix)
+    return MultiplyMatrix(minusMatrix(InverseMatrix(D)), InverseMatrix(matrixAddition(L, U)))
+
+
+def JacobiH(matrix):
+    """
+        :param matrix: Matrix nxn
+        :return: H matrix
+        """
+    D, L, U = matrixDLUdissasembly(matrix)
+    return InverseMatrix(D)
+
+
+def GaussSeidelG(matrix):
+    """
+            :param matrix: Matrix nxn
+            :return: G matrix
+            """
+    D, L, U = matrixDLUdissasembly(matrix)
+    return MultiplyMatrix(minusMatrix(InverseMatrix(matrixAddition(L, D))), U)
+
+
+def GaussSeidelH(matrix):
+    """
+            :param matrix: Matrix nxn
+            :return: H matrix
+            """
+    D, L, U = matrixDLUdissasembly(matrix)
+    return InverseMatrix(matrixAddition(L, D))
+
+
+
+def CheckJacobiGnorm(matrix):
+    return 1 > MaxNorm(JacobiG(matrix))
+
+
+
+def CheckGaussSeidelGnorm(matrix):
+    return 1 > MaxNorm(GaussSeidelG(matrix))
+
+
+def InitVector(size):
+    return [0 for index in range(size)]
+
