4609be86567a1ca055f4d05d4d9a4a68591d920e
Ofek Elgozi <ofeke209@gmail.com>
2022-04-24 17:25:17 +0300

    Folders + Lottery

---
+++ Lottery.py
@@ -0,0 +1,14 @@
+import time
+
+input = int(input('Enter id:'))
+
+random = (((time.localtime().tm_hour+time.localtime().tm_min)*time.localtime().tm_sec)//13) + input
+
+print((random % 12) + 19)
+
+
+
+
+
+
+
