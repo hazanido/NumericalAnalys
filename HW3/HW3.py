d3ac495d0c179711020baac3d0b16c64f286eac0
Ofek <ofekel@ac.sce.ac.il>
2022-05-14 15:22:53 +0300

    Auto stash before merge of "main" and "origin/main"

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -75,3 +75,79 @@ def NewtonsMethod(f, x0, tries=100, epsylon=0.0001, symbole=sp.symbols('x')):
     return None
 
 
+def NewtonsMethodInRangeIterations(f, check_range, tries=10, epsilon=0.0001, symbol=sp.symbols('x')):
+    """
+    This function find a root to a function by using the newton raphson method by a given list of guesses.
+    :param f: The function with sympy symbols.
+    :param check_range: List of guesses.
+    :param tries: Number of tries to find the root.
+    :param symbole: The symbol you entered in the function (Default is lower case x)
+    :param epsylon: The tolerance of the deviation of the solution ;
+    How precise you want the solution (the smaller the better).
+    :return:Returns a list roots by raphson method ,
+    if it fails to find a solutions in the given tries limit it will return an empty list .
+    """
+    roots = []
+    for i in check_range:
+        if i == check_range[-1]:
+            break
+        for sep in range(0, 10):
+            check_number = round(i + (sep * 0.1), 2)
+            print(bcolors.HEADER, "First guess:", check_number , bcolors.ENDC)
+            local_root = NewtonsMethod(f, check_number, tries, epsilon, symbol)
+            if local_root is not None and round(local_root,6) not in roots:
+                roots+=[round(local_root,6)]
+            else:
+                print(bcolors.FAIL, "Already found that root.", bcolors.ENDC)
+    return roots
+
+
+def SecantMethodInRangeIterations(f, check_range, epsilon=0.0001):
+    """
+    This function find a root to a function by using the secant method by a given list of values to check beetween.
+    :param f: The function (as a python function).
+    :param check_range: List of values to check between ; e.g (1,2,3,4,5) it will check between 1-2,2-3,....
+    :param epsylon: The tolerance of the deviation of the solution ;
+    How precise you want the solution (the smaller the better).
+    :return:Returns a list roots by secant method ,
+    if it fails to find a solutions in the given tries limit it will return an empty list .
+    """
+    roots = []
+    iterCounter = 0
+    for i in check_range:
+        if i == check_range[-1]:
+            break
+        for sep in range(0, 10):
+            startPoint = round(i + (sep * 0.1), 2)
+            endPoint = round(i + ((sep+1) * 0.1), 2)
+            print(bcolors.HEADER, "Checked range:", startPoint, "-",endPoint, bcolors.ENDC)
+            local_root = SecantMethod(f, startPoint, endPoint, epsilon, iterCounter)
+            if local_root is not None and round(local_root,6) not in roots:
+                roots += [round(local_root,6)]
+            else:
+                print(bcolors.FAIL, "Already found that root.",bcolors.ENDC)
+    return roots
+
+
+def SecantMethod(polynomial,firstGuess, secondGuess,epsilon, iterCounter):
+    """
+     This function find a root to a function by using the SecantMethod method by a given tow guess.
+    :param polynomial: The function on which the method is run
+    :param firstGuess: The first guess
+    :param secondGuess: The second guess
+    :param epsilon: The tolerance of the deviation of the solution
+    :param iterCounter: number of tries until the function found the root.
+    :return:Returns the local root by Secant method ,
+    if it fails to find a solutions in the given tries limit it will return None .
+    """
+    if iterCounter > 100:
+        return
+
+    if abs(secondGuess - firstGuess) < epsilon: #Stop condition
+        print("after ", iterCounter, "iterations The root found is: ", bcolors.OKBLUE, round(secondGuess, 6), bcolors.ENDC)
+        return round(secondGuess, 6) # Returns the root found
+
+    next_guess = (firstGuess * polynomial(secondGuess) - secondGuess * polynomial(firstGuess))/(polynomial(secondGuess) - polynomial(firstGuess))
+
+    return SecantMethod(polynomial, secondGuess, next_guess, epsilon, iterCounter+1)
+
